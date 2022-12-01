ab6f41052d31e222ea594b4e256880b0ad55604c
Szymon Kaczorowski <szymon.kaczorowski@kevin.eu>
2022-12-01 07:27:08 +0100

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,10 +1,35 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var max = 0;
+        var current = 0;
+        for (line in input) {
+            if (line.isEmpty()) current = 0 else
+                current += line.toInt()
+
+            if (current > max) max = current
+        }
+        return max
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val maxSet = mutableSetOf(0)
+        var current = 0;
+        for (line in input) {
+            if (line.isEmpty()) {
+                if (maxSet.any { it<current }){
+                    maxSet+=current
+                    if (maxSet.size>3){
+                        maxSet.remove(maxSet.min())
+
+                    }
+                }
+                current = 0
+            } else
+                current += line.toInt()
+
+
+        }
+        return maxSet.sum()
     }
 
     // test if implementation meets criteria from the description, like:
